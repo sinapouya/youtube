8d2f43c35fe64eddeba601d1151eb79c5acde969
sinapouya <pooya.sina@gmail.com>
2020-03-05 11:05:01 +0330

    Updates

---
+++ precache-manifest.841979a0fbd7ea2019bb6e0129b04d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6684bd4f5e5eb895f4ab2cfa33e278",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "a89e10edfd1395e11b09",
+    "url": "/youtube/static/css/main.a29f22d4.chunk.css"
+  },
+  {
+    "revision": "f41aaf409d1633023890",
+    "url": "/youtube/static/js/2.d75ce55e.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/youtube/static/js/2.d75ce55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89e10edfd1395e11b09",
+    "url": "/youtube/static/js/main.9993ca54.chunk.js"
+  },
+  {
+    "revision": "7b3f894f1ce41076ddbd",
+    "url": "/youtube/static/js/runtime-main.915c392f.js"
+  }
+]);
